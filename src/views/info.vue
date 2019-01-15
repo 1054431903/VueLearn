3858bdbc40a82dc15aad33303cecc6f2685be34d
jiaqi <jiaqi@zenmen.com>
2019-01-15 11:01:25 +0800

    testVue1

---
+++ src/views/info.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        HELLO INFO componet
+        <button type="button" @click="add()">add</button>
+        <div>
+            {{msg}}
+        </div>
+    </div>
+</template>
+<script>
+import store from '@/store'
+export default {
+    name: 'Info',
+    data() {
+        return {
+            msg:'my vue '
+        }
+    },  
+    store,
+    methods: {
+        add(){
+            console.log('add an elements');
+            store.commit('increase');
+        },
+    },
+}
+</script>   
+<style lang="">
+    
+</style>
